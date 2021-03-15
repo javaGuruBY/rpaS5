1f98795dcc2a41ab22189e0c357dfc7e895d9745
2Rman <i_rau90@.mail.ru>
2021-03-16 01:24:33 +0300

    Math operations demonstration classes. Homework 1 task 1

---
+++ homework/java/Igor_Antonovich/Lecture1_task1/src/main/java/by/jrr/syntax/calculations/Addition.java
@@ -0,0 +1,8 @@
+package by.jrr.syntax.calculations;
+
+public class Addition {
+
+    public int calculate(int a, int b) {
+        return (a + b);
+    }
+}
---
+++ homework/java/Igor_Antonovich/Lecture1_task1/src/main/java/by/jrr/syntax/calculations/Subtraction.java
@@ -0,0 +1,9 @@
+package by.jrr.syntax.calculations;
+
+public class Subtraction {
+
+    public int calculate(int a, int b) {
+        
+        return (a - b);
+    }
+}

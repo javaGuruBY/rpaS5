97f228e03cbcfe0b4954ceb7f099ccc14e920a6b
Access42code <ele.chekis@gmail.com>
2021-03-16 22:07:31 +0200

    Add new file

---
+++ homework/java/chekis_elena/blog/src/main/java/by/jrr/start/Application.java
@@ -0,0 +1,67 @@
+package by.jrr.start;
+
+public class MathOperationsDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the 'MathOperationsDemo'!");
+
+        int a = 200;
+        int b = 60;
+
+        System.out.println("1) Addition '+':");
+        System.out.print("a = ");
+        System.out.print(a);
+        System.out.print(", ");
+        System.out.print("b = ");
+        System.out.print(b); /
+
+        int sum = a + b;
+        System.out.println();
+        System.out.print("a + b = ");
+        System.out.print(a);
+        System.out.print(" + ");
+        System.out.print(b);
+        System.out.print(" = ");
+        System.out.print(sum);
+    }
+
+}
+
+public class MathOperationsDemo {
+
+    public static void main(String[] args) {
+        System.out.println("2) Subtraction '-':");
+        long x = 50;
+        long y = 16;
+        System.out.println("x = " + x + ", y = " + y);
+
+        long difference = x - y;
+        System.out.println("x - y = " + x + " - " + y + " = " + difference);
+    }
+
+}
+
+public class MathOperationsDemo {
+
+    public static void main(String[] args) {
+        System.out.println("2) Division '/':");
+        short m = 14;
+        short n = 2;
+        System.out.println("m = " + m + ", n = " + n);
+        System.out.println("m / n = " + m + " / " + n + " = " + (14 / 2));
+    }
+
+}
+public class MathOperationsDemo {
+
+    public static void main(String[] args)
+System.out.println("2) Division '*':");
+    int a = 10;
+    int b = 7;
+    int c = a * b;  // 70
+    int d = b * 5;  // 35
+
+}
+
+}
---
+++ homework/java/chekis_elena/task_2/src/main/java/by/jrr/start/servece/Simple.java
@@ -0,0 +1,8 @@
+package by.jrr.start.servece;
+
+public class Simple {
+    public string DuMMyR;
+
+    private void STUGG (List)
+
+}

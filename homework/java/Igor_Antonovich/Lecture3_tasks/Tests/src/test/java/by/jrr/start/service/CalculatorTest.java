1d46775ae277f606de02ba7d22a464ef69e5db42
2Rman <i_rau90@.mail.ru>
2021-03-19 20:03:34 +0300

    Test by hands, tests by JUnit repeated

---
+++ homework/java/Igor_Antonovich/Lecture3_tasks/Tests/src/test/java/by/jrr/start/service/CalculatorTest.java
@@ -0,0 +1,45 @@
+package by.jrr.start.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testSum() throws Exception{
+        int a = 5;
+        int b = 10;
+        int expected = 15;
+
+        var calculator = new Calculator();
+        int actual = calculator.sum(a, b);
+
+        assertEquals( "testSum", expected, actual);
+
+    }
+
+    @Test
+    public void testSubs() throws Exception {
+        int a = 5;
+        int b = 10;
+        int expected = -5;
+
+        var calculator = new Calculator();
+        int actual = calculator.subs(a, b);
+
+        assertEquals( "testSubs", expected, actual);
+    }
+
+    @Test
+    public void testMult() throws Exception {
+        int a = 5;
+        int b = 10;
+        int expected = 50;
+
+        var calculator = new Calculator();
+        int actual = calculator.subs(a, b);
+
+        assertEquals("testMult", expected, actual );
+    }
+}

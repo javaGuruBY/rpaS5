a03846f98c669a71b16ca139cdb95651b96c2de7
Parsleyka <grigorashnazar@gmail.com>
2021-03-20 17:57:30 +0200

    Task3 from lecture 3 has been doone

---
+++ homework/java/hryhorash_nazarii/lecture3/task3/src/main/java/by/jrr/start/ProductDemo.java
@@ -0,0 +1,9 @@
+package by.jrr.start;
+
+public class ProductDemo {
+
+    public static void main(String[] args){
+        Product product = new Product("Milk", 0.8,20);
+        product.printInformation();
+    }
+}

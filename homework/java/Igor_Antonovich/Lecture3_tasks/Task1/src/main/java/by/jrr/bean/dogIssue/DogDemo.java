fe1093fee9a17d1989be3cb09c65ae1676763147
2Rman <i_rau90@.mail.ru>
2021-03-22 19:11:57 +0300

    classes Dog, DogDemo created

--- homework/java/Igor_Antonovich/Lecture3_tasks/Task1/src/main/java/by/jrr/bean/dogIssue/DogDemo.java
+++ homework/java/Igor_Antonovich/Lecture3_tasks/Task1/src/main/java/by/jrr/bean/dogIssue/DogDemo.java
@@ -11,8 +11,8 @@ public class DogDemo {
         Dog dog2 = new Dog(4, "White", "Clyde");
 
         System.out.println("Dogs representation: ");
-        System.out.println(dog1.getName() + " is " + dog1.getAge() + " and she is" + dog1.getColor());
-        System.out.println(dog2.getName() + " is " + dog2.getAge() + " and he is" + dog2.getColor());
+        System.out.println(dog1.getName() + " is " + dog1.getAge() + " and she is " + dog1.getColor());
+        System.out.println(dog2.getName() + " is " + dog2.getAge() + " and he is " + dog2.getColor());
 
         System.out.println("Dogs actions:");
         dog1.voice();

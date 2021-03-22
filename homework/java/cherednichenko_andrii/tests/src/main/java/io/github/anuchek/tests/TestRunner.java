5e40e7d7f83116f1a4911651ac4dcc9209e93f48
Anuchek <andrii240840@gmail.com>
2021-03-22 17:55:47 +0200

    test from lecture 2

---
+++ homework/java/cherednichenko_andrii/tests/src/main/java/io/github/anuchek/tests/TestRunner.java
@@ -0,0 +1,48 @@
+package io.github.anuchek.tests;
+
+public class TestRunner {
+
+    static int totalTests  = 0;
+//    static int testsPassed = 0;
+//    static int testsFailed = 0;
+//
+//    public static void runTests() throws Exception {
+//        var calculatorTests = new CalculatorTests();
+//        try {
+//            totalTests++;
+//            calculatorTests.testSum();
+//            testsPassed++;
+//        } catch (Exception ex){
+//            testsFailed++;
+//            System.out.println(ex.getMessage());
+//            ex.printStackTrace();
+//        }
+//
+//        try {
+//            totalTests++;
+//            calculatorTests.testSubs();
+//            testsPassed++;
+//        } catch (Exception ex){
+//            testsFailed++;
+//            System.out.println(ex.getMessage());
+//            ex.printStackTrace();
+//        }
+//
+//        try {
+//            totalTests++;
+//            calculatorTests.testMult();
+//            testsPassed++;
+//        } catch (Exception ex){
+//            testsFailed++;
+//            System.out.println(ex.getMessage());
+//            ex.printStackTrace();
+//        }
+//
+//        System.out.println("Total Tests: " + totalTests);
+//        System.out.println("Tests Failed: " + testsFailed);
+//        System.out.println("Tests Passed: " + testsPassed);
+//        if ( testsFailed > 0){
+//            throw new Exception("test failed");
+//        }
+//    }
+}

2af9cffe61c28bbe4644ab262dc9cbc158af8440
Anuchek <andrii240840@gmail.com>
2021-03-22 19:03:08 +0200

    adds checkUserPassword functionality

--- homework/java/cherednichenko_andrii/tdd/src/main/java/io/github/anuchek/service/LoginService.java
+++ homework/java/cherednichenko_andrii/tdd/src/main/java/io/github/anuchek/service/LoginService.java
@@ -1,5 +1,10 @@
 package io.github.anuchek.service;
 
+import io.github.anuchek.bean.User;
+
 public class LoginService {
 
+    public boolean checkUserPassword(User user, String userInput) {
+        return user.getPassword().equals(userInput);
+    }
 }

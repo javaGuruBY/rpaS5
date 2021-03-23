ae187cae3029b987db5efe16303507791949a133
natal-lia-b <natallia.b@bk.ru>
2021-03-24 01:24:48 +0300

    add initial reduce login attempt on wrong password

--- homework/java/natal-lia-b/hw2/hw2_project2_ms_login_feature/src/main/java/by/jrr/service/LoginService.java
+++ homework/java/natal-lia-b/hw2/hw2_project2_ms_login_feature/src/main/java/by/jrr/service/LoginService.java
@@ -6,4 +6,13 @@ public class LoginService {
     public boolean checkUserService(User user, String userInput) {
         return user.getPassword().equals(userInput);
     }
+
+    public void reduceLoginAttempts(User user) {
+        user.setLoginAttempts(user.getLoginAttempts() - 1) ;
+    }
+
+    public boolean login(User user, String userInput) {
+        reduceLoginAttempts(user);
+        return checkUserService(user, userInput);
+    }
 }

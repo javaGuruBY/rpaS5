35a045fff645f8b51a2725409573c2b901edb7e9
2Rman <i_rau90@.mail.ru>
2021-03-23 12:03:27 +0300

    classes Encoder, EncoderTests created

--- homework/java/Igor_Antonovich/Lecture3_tasks/Level2/src/main/java/by/jrr/bean/Encoder.java
+++ homework/java/Igor_Antonovich/Lecture3_tasks/Level2/src/main/java/by/jrr/bean/Encoder.java
@@ -4,45 +4,13 @@ import java.util.Objects;
 
 public class Encoder {
 
-    short number;
-    char symbol;
-
-    public short getNumber() {
-        return number;
+    public static char encode(short code) {
+        return (char) code;
     }
 
-    public void setNumber(short number) {
-        this.number = number;
-    }
-
-    public char getSymbol() {
-        return symbol;
-    }
-
-    public void setSymbol(char symbol) {
-        this.symbol = symbol;
+    public static short decode(char symbol) {
+        return (short) symbol;
     }
 
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Encoder encoder = (Encoder) o;
-        return number == encoder.number &&
-                symbol == encoder.symbol;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(number, symbol);
-    }
-
-    @Override
-    public String toString() {
-        return "Encoder{" +
-                "number=" + number +
-                ", symbol=" + symbol +
-                '}';
-    }
 }
---
+++ homework/java/Igor_Antonovich/Lecture3_tasks/Level2/src/test/java/EncoderTests.java
@@ -0,0 +1,34 @@
+import by.jrr.bean.Encoder;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EncoderTests {
+
+    @Test
+    public void decodingShouldBeRight_positive() {
+        short actualResult = Encoder.decode('F');
+
+        Assert.assertEquals(70, actualResult);
+    }
+
+    @Test
+    public void decodingShouldBeRight_negative() {
+        short actualResult = Encoder.decode('F');
+
+        Assert.assertNotEquals(1, actualResult);
+    }
+
+    @Test
+    public void encodingShouldBeRight_positive() {
+        char actualResult = Encoder.encode((short)80);
+
+        Assert.assertEquals('P', actualResult);
+    }
+
+    @Test
+    public void encodingShouldBeRight_negative() {
+        char actualResult = Encoder.encode((short)80);
+
+        Assert.assertNotEquals('q', actualResult);
+    }
+}

ff3ba4d5c2f597e4825b06874e7c20ca1b0b3db2
2Rman <i_rau90@.mail.ru>
2021-03-23 13:48:14 +0300

    priceIsPositive, discountIsValid tests created

--- homework/java/Igor_Antonovich/Lecture3_tasks/Level3/src/test/java/ProductTests.java
+++ homework/java/Igor_Antonovich/Lecture3_tasks/Level3/src/test/java/ProductTests.java
@@ -5,14 +5,34 @@ import org.junit.Test;
 public class ProductTests {
     
     @Test
+    public void priseIsPositiveTest() {
+        Product testProduct = getProduct();
+        boolean actualResult = testProduct.getRegularPrice() > 0;
+
+        Assert.assertTrue(actualResult);
+    }
+
+    @Test
+    public void discountIsValidTest() {
+        Product testProduct = getProduct();
+        boolean actualResult = testProduct.getDiscount() > 0 && testProduct.getDiscount() < 100;
+
+        Assert.assertTrue(actualResult);
+    }
+
+    @Test
     public void discountTest() {
-        
-        Product product = new Product("Apple");
-        product.setRegularPrice(100.0);
-        product.setDiscount(10.0);
-        
+        Product product = getProduct();
         double actualResult = product.actualPrice(product.getRegularPrice(),product.getDiscount());
 
         Assert.assertEquals(90.0, actualResult, 0.01);
+    }
+    
+    private Product getProduct() {
+        Product testProduct = new Product("Apple");
+        testProduct.setRegularPrice(100.0);
+        testProduct.setDiscount(10.0);
+        
+        return testProduct;
     }
 }
